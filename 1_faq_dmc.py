6827862ecdfd14a9faa89373c3bed7eb8a0431ed
Claire Lozano <claire.lozano@live.fr>
2018-01-15 11:15:19 +0100

    read json function

--- 1_faq_dmc.py
+++ 1_faq_dmc.py
@@ -6,11 +6,11 @@ from pprint import pprint
 def readJson(data):
 	data = json.load(open(data))
 	reponseList = {} 
-	reponseList["0"] = []
 	reponseList["1"] = []
 	reponseList["2"] = []
 	reponseList["3"] = []
 	reponseList["4"] = []
+	reponseList["5"] = []
 
 	return data, reponseList
 
